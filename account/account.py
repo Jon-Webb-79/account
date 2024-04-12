6154b362f2fc8d47ce10b6a6a8f0901fc03213f0
Jon Webb <webbja123@gmail.com>
2024-04-12 16:54:04 -0600

    Added Plots

--- account/account.py
+++ account/account.py
@@ -5,6 +5,11 @@ import os
 import tempfile
 
 import dash
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
 from dash import Dash, Input, Output, State, dash_table, dcc, html
 from dash.dependencies import ALL
 from dash.exceptions import PreventUpdate
@@ -23,6 +28,9 @@ from db import create_funds_df, create_position_df
 # CALLBACK FUNCTIONS
 
 app = Dash(__name__)
+
+
+pio.templates.default = "seaborn"
 
 
 @app.callback(
@@ -91,13 +99,11 @@ def display_fund_data(n_clicks_list, btn_id_list, db_data):
 
     db_path = db_data["dbPath"]
     df = create_position_df(db_path, fund_name)
-    print(df)
 
     # Assuming df is your DataFrame for the specific fund
     df["Credit"] = df["Credit"].apply(lambda x: f"${x:,.2f}")
     df["Close"] = df["Close"].apply(lambda x: f"${x:,.2f}")
     df["Date"] = df["Date"].dt.strftime("%Y-%m-%d")
-    print(df)
     # Display the specific columns
     return dash_table.DataTable(
         data=df.to_dict("records"),
@@ -106,16 +112,160 @@ def display_fund_data(n_clicks_list, btn_id_list, db_data):
             {"name": "Credit ($)", "id": "Credit"},
             {"name": "Close ($)", "id": "Close"},
         ],
-        style_table={"overflowX": "auto"},
+        fixed_rows={"headers": True},
+        style_table={"overflowY": "auto", "maxHeight": "800px"},
         style_header={
             "fontWeight": "bold",  # Makes header text bold
             "textAlign": "center",  # Optional: centers the header text
+            "backgroundColor": "rgb(200, 200, 200)",
+            "color": "black",  # Text color
+            "fontSize": "20px",
+            "fontFamily": "Arial",
         },
         style_cell={
             "textAlign": "center",  # Optional:
             "padding": "10px",  # Optional:
         },
+        # If you want to specifically style only data cells and not headers:
+        style_data={
+            "fontSize": "16px",  # Change font size for data cells
+            "fontFamily": "Arial",  # Change font type for data cells
+        },
     )
+
+
+# ------------------------------------------------------------------------------------------
+
+
+@app.callback(
+    Output("close-price-plot", "figure"),
+    [Input({"type": "fund-button", "index": ALL}, "n_clicks")],
+    [State({"type": "fund-button", "index": ALL}, "id"), State("db-path", "data")],
+)
+def update_graph(n_clicks_list, btn_id_list, db_data):
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        raise dash.exceptions.PreventUpdate
+
+    # Extract fund name from the button ID that was clicked
+    triggered_id = ctx.triggered[0]["prop_id"].split(".")[0]
+    fund_name = json.loads(
+        triggered_id.replace("fund-button-", "")
+    )  # Adjust this based on actual ID format
+
+    if db_data is None:
+        raise dash.exceptions.PreventUpdate
+
+    db_path = db_data["dbPath"]
+    name = fund_name["index"]
+    df = create_position_df(db_path, fund_name["index"])
+
+    # Create the plot
+    fig = px.line(
+        df,
+        x="Date",
+        y="Close",
+        title=f"{name} Close Price Time History",
+        template="seaborn",
+    )
+    fig.update_layout(
+        title={
+            "text": "<b>Close Price Time History</b>",
+            "y": 0.9,
+            "x": 0.5,
+            "xanchor": "center",
+            "yanchor": "top",
+            "font": {"family": "Arial", "size": 24, "color": "black"},
+        },
+        xaxis_title="Date",
+        yaxis_title="Close ($)",
+        font=dict(family="Courier New, monospace", size=30, color="black"),
+        xaxis=dict(
+            title_font=dict(size=22, family="Courier New, monospace"),
+            tickfont=dict(size=18, family="Courier New, monospace"),
+        ),
+        yaxis=dict(
+            title_font=dict(size=22, family="Courier New, monospace"),
+            tickfont=dict(size=18, family="Courier New, monospace"),
+        ),
+        autosize=False,
+        #    width=800,
+        height=800,
+    )
+    return fig
+
+
+# ------------------------------------------------------------------------------------------
+
+
+@app.callback(
+    Output("candlestick-plot", "figure"),
+    [Input({"type": "fund-button", "index": ALL}, "n_clicks")],
+    [State({"type": "fund-button", "index": ALL}, "id"), State("db-path", "data")],
+)
+def update_candlestick(n_clicks_list, btn_id_list, db_data):
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        raise dash.exceptions.PreventUpdate
+
+    # Extract fund name from the button ID that was clicked
+    triggered_id = ctx.triggered[0]["prop_id"].split(".")[0]
+    fund_name = json.loads(
+        triggered_id.replace("fund-button-", "")
+    )  # Adjust this based on actual ID format
+
+    if db_data is None:
+        raise dash.exceptions.PreventUpdate
+
+    db_path = db_data["dbPath"]
+    df = create_position_df(db_path, fund_name["index"])
+
+    # Preparing data for the plot
+    df["Date"] = pd.to_datetime(df["Date"])  # Ensure date is datetime type
+    df.sort_values("Date", inplace=True)
+    df["PrevPercentage"] = df["Percentage"].shift(1).fillna(df["Percentage"][0])
+    df["PercentChange"] = df["Percentage"] - df["PrevPercentage"]
+    df["color"] = np.where(df["PercentChange"] >= 0, "green", "red")
+
+    # Create the candlestick-like plot
+    fig = go.Figure(
+        data=[
+            go.Bar(
+                x=df["Date"],
+                y=df["PercentChange"],
+                base=df["PrevPercentage"],
+                marker_color=df["color"],
+                hoverinfo="text",
+                text=df["PercentChange"].apply(lambda x: f"{x:.2f}%"),
+            )
+        ]
+    )
+
+    fig.update_layout(
+        title={
+            "text": "<b>Account Change in %</b>",
+            "y": 0.9,
+            "x": 0.5,
+            "xanchor": "center",
+            "yanchor": "top",
+            "font": {"family": "Arial", "size": 24, "color": "black"},
+        },
+        xaxis_title="Date",
+        yaxis_title="Delta (%)",
+        font=dict(family="Courier New, monospace", size=30, color="black"),
+        xaxis=dict(
+            title_font=dict(size=22, family="Courier New, monospace"),
+            tickfont=dict(size=18, family="Courier New, monospace"),
+        ),
+        yaxis=dict(
+            title_font=dict(size=22, family="Courier New, monospace"),
+            tickfont=dict(size=18, family="Courier New, monospace"),
+        ),
+        autosize=False,
+        #    width=800,
+        height=800,
+    )
+    return fig
 
 
 # ==========================================================================================
@@ -161,15 +311,24 @@ app.layout = html.Div(
                 html.Div(id="funds-buttons"),  # This will hold the fund buttons
                 # Container for displaying the table below the buttons
                 html.Div(
-                    id="table-container", style={"width": "50%", "padding-top": "20px"}
+                    id="table-container",
+                    style={"width": "100%", "padding-top": "20px"},
                 ),
             ],
             className="left-column",
-            style={"width": "50%", "display": "inline-block", "verticalAlign": "top"},
+            style={"width": "100%", "display": "inline-block", "verticalAlign": "top"},
         ),
         # Right column for other content
         html.Div(
-            [html.P("This is the right column content.")],
+            [
+                # Add the graph at the top of the right column
+                dcc.Graph(
+                    id="close-price-plot",
+                ),
+                dcc.Graph(
+                    id="candlestick-plot",
+                ),
+            ],
             className="right-column",
             style={"width": "70%", "display": "inline-block", "verticalAlign": "top"},
         ),
