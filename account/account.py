ab145112c6b4b27d84cf482def6183cbfde9ec92
Jon Webb <webbja123@gmail.com>
2024-04-15 20:07:19 -0600

    Removed old_account.py file

--- account/account.py
+++ account/account.py
@@ -2,6 +2,8 @@
 
 import os
 
+import plotly.io as pio
+from button_callbacks import ButtonCallbackManager, register_button_callbacks
 from dash import Dash
 from layout import create_layout
 
@@ -20,52 +22,11 @@ from layout import create_layout
 # Set the path to the assets folder and instantiate Dash object
 assets_folder = os.path.join(os.getcwd(), "../data", "assets")
 app = Dash(__name__, assets_folder=assets_folder, title="Brokerage Data")
+app.config.suppress_callback_exceptions = True
+pio.templates.default = "seaborn"
+# Create layout
 app.layout = create_layout(app)
-# app = Dash(__name__)
-
-
-# app.layout = html.Div(
-#     [
-#         # Provides a variable to which the database file name can be assigned
-#         dcc.Store(id="db-path"),
-#         # Left column for file upload, buttons, and the table
-#         html.Div(
-#             [
-#                 # File upload component
-#                 dcc.Upload(
-#                     id="upload-file",
-#                     children=html.Button("Upload File", className="upload-button"),
-#                     multiple=False,
-#                 ),
-#                 # Container for displaying file info and buttons
-#                 html.Div(id="file-info"),
-#                 html.Div(id="funds-buttons"),  # This will hold the fund buttons
-#                 # Container for displaying the table below the buttons
-#                 html.Div(
-#                     id="table-container",
-#                     style={"width": "100%", "padding-top": "20px"},
-#                 ),
-#             ],
-#             className="left-column",
-#             style={"width": "100%", "display": "inline-block", "verticalAlign": "top"},
-#         ),
-#         # Right column for other content
-#         html.Div(
-#             [
-#                 # Add the graph at the top of the right column
-#                 dcc.Graph(
-#                     id="close-price-plot",
-#                 ),
-#                 dcc.Graph(
-#                     id="candlestick-plot",
-#                 ),
-#             ],
-#             className="right-column",
-#             style={"width": "70%", "display": "inline-block", "verticalAlign": "top"},
-#         ),
-#     ],
-#     id="app-container",
-# )
+register_button_callbacks(app, ButtonCallbackManager())
 # ==========================================================================================
 # ==========================================================================================
 
